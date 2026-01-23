e4415c6178de2115133730fc85ca93af687e65cb
Roman Kvas <rk.sambir@gmail.com>
2026-01-23 13:13:22 +0200

    123

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 let history = JSON.parse(localStorage.getItem('myFiles') || '[]');
 
-// Оновлення інтерфейсу історії
+
 function updateHistoryUI() {
     const container = $('#history');
     container.empty();
@@ -23,14 +23,14 @@ function updateHistoryUI() {
     });
 }
 
-// Видалення конкретного запису з історії (тільки в браузері)
+
 window.removeFromHistory = function(pin) {
     history = history.filter(f => f.pin != pin);
     localStorage.setItem('myFiles', JSON.stringify(history));
     updateHistoryUI();
 };
 
-// Завантаження файлу
+
 $('#uploadBtn').click(function () {
     const fileInput = document.getElementById('file');
     if (!fileInput.files.length) return alert('Choose file');
@@ -47,7 +47,7 @@ $('#uploadBtn').click(function () {
             $('#pin-display').text(pin);
             $('#result-area').show();
             
-            // QR Code
+         
             $('#qrcode').empty();
             new QRCode(document.getElementById("qrcode"), {
                 text: window.location.origin + "/download/" + pin,
@@ -55,7 +55,6 @@ $('#uploadBtn').click(function () {
                 height: 128
             });
 
-            // Додаємо в історію
             history.unshift({ name: fileInput.files[0].name, pin: pin });
             if(history.length > 5) history.pop();
             localStorage.setItem('myFiles', JSON.stringify(history));
@@ -72,18 +71,18 @@ $('#uploadBtn').click(function () {
         });
 });
 
-// Скачування за ПІНом
+
 $('#downloadBtn').click(function () {
     const pin = $('#pin').val();
     if (pin) window.location.href = `/download/${pin}`;
     else alert('Enter PIN');
 });
 
-// Статистика
+
 function loadStats() {
     axios.get('/stats').then(res => $('#totalFiles').text('Files online: ' + res.data.totalFiles));
 }
 
-// Початкова ініціалізація
+
 loadStats();
 updateHistoryUI();
